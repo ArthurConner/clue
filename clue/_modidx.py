1f4aa271be19e4dfb809ae59e5e5f7272fe43004
arthurconner <aconner@earthlink.net>
2023-11-28 16:08:39 -0800

    more interface

--- clue/_modidx.py
+++ clue/_modidx.py
@@ -5,4 +5,8 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://ArthurConner.github.io',
                 'git_url': 'https://github.com/ArthurConner/clue',
                 'lib_path': 'clue'},
-  'syms': {'clue.core': {'clue.core.foo': ('core.html#foo', 'clue/core.py')}}}
+  'syms': { 'clue.core': { 'clue.core.cIndentity': ('core.html#cindentity', 'clue/core.py'),
+                           'clue.core.cardFull': ('core.html#cardfull', 'clue/core.py'),
+                           'clue.core.cardShort': ('core.html#cardshort', 'clue/core.py'),
+                           'clue.core.chooseCard': ('core.html#choosecard', 'clue/core.py'),
+                           'clue.core.printHand': ('core.html#printhand', 'clue/core.py')}}}
