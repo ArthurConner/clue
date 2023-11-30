3b6e432cda20b5cbb6121723b3b76e0aa741f13a
arthurconner <aconner@earthlink.net>
2023-11-30 15:48:44 -0800

    now can reach study

--- clue.py
+++ clue.py
@@ -1,5 +1,5 @@
 import clue.gameplay as clue
 
 
-strategies = [("user",True,clue.getPlay),("simple",False,clue.simpleGuess),("simple",False,clue.simpleGuess)]
-clue.runVisualGame(4,strategies)
+strategies = [("user",True,clue.getPlay),("simple",False,clue.simpleGuess),("simple",False,clue.simpleGuess),("simple",False,clue.simpleGuess),("simple",False,clue.simpleGuess)]
+clue.runVisualGame(len(strategies),strategies)
