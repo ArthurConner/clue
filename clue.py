5d452015356ede4f7b439d7cfbfb949ac5dc2253
arthurconner <aconner@earthlink.net>
2023-12-01 13:02:51 -0800

    better guessing

--- clue.py
+++ clue.py
@@ -1,5 +1,7 @@
 import clue.gameplay as clue
 
-
-strategies = [("user",True,clue.getPlay),("simple",False,clue.simpleGuess),("simple",False,clue.simpleGuess),("simple",False,clue.simpleGuess),("simple",False,clue.simpleGuess)]
-clue.runVisualGame(len(strategies),strategies)
+p1 = ("user",True,clue.getPlay)
+s1 = ("simple",False,clue.mimic(4,clue.rWalk))
+hr1 = ("half",False,clue.halfRight(clue.rWalk))
+strategies = [hr1,hr1,p1,s1,s1]
+clue.runVisualGame(strategies)
