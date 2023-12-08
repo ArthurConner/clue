8e41fb762ea824c42709bd6fe24a8785b64d4ef7
Arthur Conner <arthurconner@me.com>
2023-12-08 07:25:20 -0800

    ui

--- clue.py
+++ clue.py
@@ -3,5 +3,8 @@ import clue.gameplay as clue
 p1 = ("user",True,clue.getPlay)
 s1 = ("simple",False,clue.mimic(4,clue.rWalk))
 hr1 = ("half",False,clue.halfRight(clue.rWalk))
-strategies = [hr1,hr1,p1,s1,s1]
+s2 = ("simple r",False,clue.repeater([clue.mimic(-1,clue.rWalk) for x in range(4)]))
+b2 = ("better rZ",False,clue.repeater([clue.mimic(8,clue.rWalk) for x in range(4)]))
+
+strategies = [b2,hr1,p1,s1,s2]
 clue.runVisualGame(strategies)
