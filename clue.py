3dbf09d1048ae2ba624994c3935ce370c6b811af
arthurconner <aconner@earthlink.net>
2023-12-14 11:02:13 -0800

    weapons now active

--- clue.py
+++ clue.py
@@ -1,10 +1,14 @@
 import clue.gameplay as clue
+import sys
+ 
+# total arguments
+n = len(sys.argv)
 
-p1 = ("user",True,clue.getPlay)
-s1 = ("simple",False,clue.mimic(4,clue.rWalk))
-hr1 = ("half",False,clue.halfRight(clue.rWalk))
-s2 = ("simple r",False,clue.repeater([clue.mimic(-1,clue.rWalk) for x in range(4)]))
-b2 = ("better rZ",False,clue.repeater([clue.mimic(8,clue.rWalk) for x in range(4)]))
+if n > 1:
+	x = sys.argv[1]
+	print(x)
+	clue.quickGame(int(x))
+else:
+	clue.quickGame(4)
+	
 
-strategies = [b2,hr1,p1,s1,s2]
-clue.runVisualGame(strategies)
